dbed8ac4682cf8d79709dee02601b00ae570ab26
Shallum Mbugua <mbuguashallom@gmail.com>
2026-03-27 19:46:24 +0300

    debugging with the debugger statement

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,17 +1,14 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
     let totalPrice = 0;
-    debugger
-    for (let i = 1; i < quantity; i++) {
+
+    // Fix: Start at i = 0 instead of i = 1
+    for (let i = 0; i < quantity; i++) { 
         totalPrice += pricePerItem;
     }
 
     if (quantity >= 10) {
-        totalPrice *= 0.9;
+        totalPrice *= 0.9; // Apply 10% discount
     }
 
     return totalPrice;
 }
-
-
-
-module.exports = calculateDiscountedPrice;
